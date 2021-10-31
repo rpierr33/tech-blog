385ec17037fe15e8aa5144ff00baa01ace126734
mrralphpierre <mrralphpierre@gmail.com>
2021-10-31 14:35:48 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize('tech_blog_db', 'root', 'password', {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
